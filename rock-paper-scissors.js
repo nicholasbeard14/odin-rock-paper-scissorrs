290557ee6b35fcaa0672e719f23f9293b6909e9f
Nick Beard <nick.beard14@hotmail.com>
2021-12-04 21:38:49 -0700

    Added UI to rock-paper-scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,112 +7,89 @@ const computerPlay = () => {
     return computerChoice
 }
 
-const playRound = (playerChoice, computerChoice) => {
-    let playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.toLowerCase().slice(1);
+const reportRoundTied = (selection) => {
+    const roundResult = document.querySelector('#current-game-result');
+
+    roundResult.textContent = "This round is a tie! " + selection + " ties with " + selection;
+}
+
+const reportRoundWonByUser = (playerSelection, computerSelection) => {
+    const roundResult = document.querySelector('#current-game-result');
+    const userScore = document.querySelector('#user-score');
+
+    roundResult.textContent = `You win this round! ${playerSelection} beats ${computerSelection}`;
+    userScore.textContent = (parseInt(userScore.textContent) + 1).toString();
+}
+
+const reportRoundWonByComputer = (playerSelection, computerSelection) => {
+    const roundResult = document.querySelector('#current-game-result');
+    const computerScore = document.querySelector('#computer-score');
+
+    roundResult.textContent = `You lose this round! ${computerSelection} beats ${playerSelection}`;
+    computerScore.textContent = (parseInt(computerScore.textContent) + 1).toString();
+}
+
+const checkWinner = () => {
+    const userScore = parseInt(document.querySelector('#user-score').textContent);
+    const computerScore = parseInt(document.querySelector('#computer-score').textContent);
+
+    const finalResult = document.querySelector('#final-result');
+
+    if (userScore == 5) {
+        finalResult.textContent = "You win the series! Refresh the page to play again!"
+        buttons.forEach(button => button.removeEventListener('click', playRound));
+    } else if (computerScore == 5) {
+        finalResult.textContent = "You lost this time. Refresh to try again!"
+        buttons.forEach(button => button.removeEventListener('click', playRound));
+    } else {
+        return
+    }
+}
+
+const playRound = (event) => {
+    const computerChoice = computerPlay();
+    console.log(computerChoice);
+
+    const playerChoice = event.target.id;
+    console.log(playerChoice);
     
-    switch (playerSelection) {
+    switch (playerChoice) {
         case 'Rock':
             if (computerChoice == 'Rock') {
-                console.log("This round is a tie! " + playerSelection + " ties with " + computerChoice);
-                return "Tie"
+                reportRoundTied(playerChoice);
             } else if (computerChoice == 'Paper') {
-                console.log(`You lose this round! ${computerChoice} beats ${playerSelection}`);
-                return "ComputerW"
+                reportRoundWonByComputer(playerChoice, computerChoice);
             } else {
-                console.log(`You win this round! ${playerSelection} beats ${computerChoice}`);
-                return "UserW"
+                reportRoundWonByUser(playerChoice, computerChoice);
             }
+            break;
         case 'Paper':
             if (computerChoice == 'Rock') {
-                console.log(`You win this round! ${playerSelection} beats ${computerChoice}`);
-                return "UserW"
+                reportRoundWonByUser(playerChoice, computerChoice);
             } else if (computerChoice == 'Paper') {
-                console.log("This round is a tie! " + playerSelection + " ties with " + computerChoice);
-                return "Tie"
+                reportRoundTied(playerChoice);
             } else {
-                console.log(`You lose this round! ${computerChoice} beats ${playerSelection}`);
-                return "ComputerW"
+                reportRoundWonByComputer(playerChoice, computerChoice);
             }
+            break;
         case 'Scissors':
             if (computerChoice == 'Rock') {
-                console.log(`You lose this round! ${computerChoice} beats ${playerSelection}`);
-                return "ComputerW"
+                reportRoundWonByComputer(playerChoice, computerChoice);
             } else if (computerChoice == 'Paper') {
-                console.log(`You win this round! ${playerSelection} beats ${computerChoice}`);
-                return "UserW"
+                reportRoundWonByUser(playerChoice, computerChoice);
             } else {
-                console.log("This round is a tie! " + playerSelection + " ties with " + computerChoice);
-                return "Tie"
+                reportRoundTied(playerChoice);
             }
+            break;
         default:
             return "Try again. You must enter either rock, paper, or scissors."
     }
+
+    checkWinner();
 }
 
-const game = () => {
-    let userScore = 0;
-    let computerScore = 0;
+const buttons = document.querySelectorAll('button');
 
-    for (let i = 0; i < 5; i++) {
-        const userChoice = prompt("Enter Rock, Paper, or Scissors ", "");
-        const computerChoice = computerPlay();
+console.log(buttons);
 
-        const result = playRound(userChoice, computerChoice);
-
-        switch (result) {
-            case 'UserW':
-                userScore++;
-                console.log()
-                break;
-            case 'ComputerW':
-                computerScore++;
-                break;
-            case 'Tie':
-                break;
-        }
-    }
-
-    if (userScore > computerScore) {
-        console.log("You win the series!");
-    } else if (computerScore > userScore) {
-        console.log("You lose the series this time. Try again!");
-    } else {
-        console.log("It's a draw!");
-    }
-}
-
-game();
-
-// This was the function used for just a single game. Needed to update return vals
-// const playRound = (playerChoice, computerChoice) => {
-//     let playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.toLowerCase().slice(1);
-    
-//     switch (playerSelection) {
-//         case 'Rock':
-//             if (computerChoice == 'Rock') {
-//                 return "It's a tie! " + playerSelection + " ties with " + computerChoice
-//             } else if (computerChoice == 'Paper') {
-//                 return `You lose! ${computerChoice} beats ${playerSelection}`
-//             } else {
-//                 return `You win! ${playerSelection} beats ${computerChoice}`
-//             }
-//         case 'Paper':
-//             if (computerChoice == 'Rock') {
-//                 return `You win! ${playerSelection} beats ${computerChoice}`
-//             } else if (computerChoice == 'Paper') {
-//                 return "It's a tie! " + playerSelection + " ties with " + computerChoice
-//             } else {
-//                 return `You lose! ${computerChoice} beats ${playerSelection}`
-//             }
-//         case 'Scissors':
-//             if (computerChoice == 'Rock') {
-//                 return `You lose! ${computerChoice} beats ${playerSelection}`
-//             } else if (computerChoice == 'Paper') {
-//                 return `You win! ${playerSelection} beats ${computerChoice}`
-//             } else {
-//                 return "It's a tie! " + playerSelection + " ties with " + computerChoice
-//             }
-//         default:
-//             return "Try again. You must enter either rock, paper, or scissors."
-//     }
-// }
+buttons.forEach(button => button.addEventListener('click', playRound));
